057d85b108000601a207473528826b4913f062e1
BVanBuren <bvanburenwx@gmail.com>
2021-07-19 17:25:10 -0400

    Changed index.html and added Jquery. Added an init for calling to the API. adding click listeners

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,22 @@
+init()
 const currentDate = moment();
+var citySearch = $("#city");
+var searchBtn = $("#search-btn");
+var cityName = $("#chosen-city");
+var cityInfo = [];
 
+
+for (var i = 0; i<localStorage.length; i++) {
+    var pastCity = localStorage.getItem(i);
+    var pastCityName = $("#past-search-buttons").addClass("past-search-city");
+
+    pastCityName.append("<li>" + pastCity + "</li>");
+}
 function init() {
-    fetchLatLon("Durham");
+    fetchGPS("Durham");
 }
 
-
-function fetchLatLon(cityInput) {
+function fetchGPS(cityInput) {
     fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=1c98b5aa11143eaafd210d8e853c0df1")
         .then(function (response) {
             return response.json();
@@ -17,7 +28,6 @@ function fetchLatLon(cityInput) {
             fetchWeather(lat, lon);
         })
 }
-
 function fetchWeather(lat, lon) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=1c98b5aa11143eaafd210d8e853c0df1")
         .then(function (response) {
@@ -26,7 +36,12 @@ function fetchWeather(lat, lon) {
         .then(function (data) {
             // Get weather icon
             console.log(data);
+
+            cityName.textcontent = '${city} (${moment().format("M/D/YYYY")})';
+
+            getForecast(data);
+            fiveday(data);
         })
 }
 
-init();
+searchBtn.click(fetchGPS);
