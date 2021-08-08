ec421d6c8a121006ed6cff3b521a695e00cf3a71
BVanBuren <bvanburenwx@gmail.com>
2021-08-08 18:01:59 -0500

    added functionality to call API when I click the search button.

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,12 @@
 init()
 const currentDate = moment();
 var citySearch = $("#city");
-var searchBtn = $("#search-btn");
+var searchBtn = $(".search-btn");
 var city = $("#chosen-city");
 var cityInfo = [];
 
-var cityInput = $(".form-input").val();
+
+
 
 
 for (var i = 0; i<localStorage.length; i++) {
@@ -15,10 +16,18 @@ for (var i = 0; i<localStorage.length; i++) {
     pastCityName.append("<li>" + pastCity + "</li>");
 }
 function init() {
-    fetchGPS("Durham");
+    // fetchGPS("Durham");
+    
 }
 
+searchBtn.on("click", function(){
+    var cityInput = $(".form-input").val();
+    console.log("button click ", cityInput)
+    fetchGPS(cityInput);
+});
+
 function fetchGPS(cityInput) {
+    console.log("city input inside fetchgps", cityInput);
     fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=1c98b5aa11143eaafd210d8e853c0df1")
         .then(function (response) {
             return response.json();
@@ -27,11 +36,16 @@ function fetchGPS(cityInput) {
             var lat = data[0].lat;
             var lon = data[0].lon;
 
-            fetchWeather(lat, lon);
+            var userCity = data[0].name;
+            console.log(data[0].name);
+            
+            fetchWeather(lat, lon, userCity);
             console.log(data)
         })
 }
-function fetchWeather(lat, lon) {
+
+function fetchWeather(lat, lon, userCity) {
+    console.log("USER CITY IN FETCH WEATHER", userCity)
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=1c98b5aa11143eaafd210d8e853c0df1")
         .then(function (response) {
             return response.json();
@@ -45,8 +59,11 @@ function fetchWeather(lat, lon) {
             function getForecast() {
                 var currentForecast = $("#currentForecast").append("<div>").addClass("forecast-body")
                 currentForecast.empty();
+                var uvData = data.current.uvi
                 var cityName = currentForecast.append("<p>");
-            
+                console.log("USER CITY IN GET FORECAST", userCity)
+                cityName.text(userCity);
+                console.log(cityName);
                 currentForecast.append(cityName);
             
                 var currentTemp = cityName.append("<p>")
@@ -59,7 +76,15 @@ function fetchWeather(lat, lon) {
 
                 currentTemp.append("<p>" + "Wind-Speed: "  + data.current.wind_speed + "</p>");
 
-                currentTemp.append("<p>" + "UV Index: "  + data.current.uvi + "</p>");
+                currentTemp.append("<p>" + "UV Index: "  + uvData + "</p>").addClass("uvIndex");
+
+                if (uvData < 4) {
+                    $(".uvIndex").addClass("favorable")
+                } else if (uvData > 4 || uvData < 9) {
+                    $(".uvIndex").addClass("moderate")
+                } else {
+                    $(".uvIndex").addClass("severe")
+                }
 
             };
         });
